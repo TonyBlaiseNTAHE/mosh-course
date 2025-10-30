412c68049d2569ba070498286596ff3250e166b0
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2025-10-30 10:47:06 +0200

    creating a react component

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+/**
+ * function the display a short message
+ */
+function Message() {
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
